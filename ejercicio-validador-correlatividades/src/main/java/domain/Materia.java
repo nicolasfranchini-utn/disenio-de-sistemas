e3e0770f7a3caaf235536a48cf34194f5e2eaf18
Nicolas Franchini <nfranchini@frba.utn.edu.ar>
2025-04-08 06:46:17 -0300

    Feat(ejercicio-validador-correlatividades): Crear clase Materia

---
+++ ejercicio-validador-correlatividades/src/main/java/domain/Materia.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Materia {
+    private String nombre;
+    private List<Materia> correlativas;
+
+    public Materia(String nombre) {
+        this.nombre = nombre;
+        this.correlativas = new ArrayList<>();
+    }
+
+    public void agregarCorrelativa(Materia materia) {
+        correlativas.add(materia);
+    }
+
+    public List<Materia> getCorrelativas() {
+        return correlativas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
