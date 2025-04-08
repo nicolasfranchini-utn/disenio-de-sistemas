2778f299e2091dd41b09b90000a6e98779b67b7c
Nicolas Franchini <nfranchini@frba.utn.edu.ar>
2025-04-08 06:44:46 -0300

    Feat(ejercicio-validador-correlatividades): Crear clase Alumno

---
+++ ejercicio-validador-correlatividades/src/main/java/domain/Alumno.java
@@ -0,0 +1,23 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Alumno {
+    private String nombre;
+    private List<Materia> materiasAprobadas;
+
+    public Alumno(String nombre) {
+        this.nombre = nombre;
+        this.materiasAprobadas = new ArrayList<>();
+    }
+
+    public void aprobar(Materia materia) {
+        materiasAprobadas.add(materia);
+    }
+
+    public boolean tieneAprobada(Materia materia) {
+        return materiasAprobadas.contains(materia);
+    }
+
+}
